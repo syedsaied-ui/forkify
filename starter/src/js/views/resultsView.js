9bc92eb5d3aac9f0bb1d99702409d45c8ca818f3
syedsaied-ui <syedsaied26@gmail.com>
2026-03-26 13:38:40 +0500

    Initial commit

---
+++ starter/src/js/views/resultsView.js
@@ -0,0 +1,17 @@
+import view from "./view.js";
+import previewView from "./previewView.js";
+import icons from "url:../../img/icons.svg";
+
+class ResultsView extends view {
+  _parentElement = document.querySelector(".results");
+  _errorMessage = "No recipes found for your query! Please try again";
+  _message = "";
+
+  _generateMarkup() {
+    console.log(this._data);
+    // return this._data.map(this._generateMarkupPreview).join("");
+    return this._data.map(result => previewView.render(result, false)).join("");
+  }
+}
+
+export default new ResultsView();
